50d6481f7151e8b2fbd5135df9e0aea04a926e39
Caroline <bancroftwanjiru10000@gmail.com>
2024-09-11 15:17:30 +0300

    python configuration

---
+++ trial.py
@@ -0,0 +1,12 @@
+# introdunction to configuration with python.
+
+'''
+there are anumber of ourside resources that we cal eleverage in working with python PPLICATIONS.
+Topics to be studies include:
+`   1. what is a module
+    2. how is a packege 
+    3. how does import allow me to expand the functinality of my application.
+    4. how to find packages on PYPI 
+    5. how is my virtual envirotnment set ip when i run the pipenv
+    
+'''
